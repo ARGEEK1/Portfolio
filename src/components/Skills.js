3613e313c1c95c2f6927fc363382f761fc27272a
ARGEEK1 <antonio198889@gmail.com>
2023-05-28 20:55:15 +0200

    [ADD] page about me

---
+++ src/components/Skills.js
@@ -0,0 +1,70 @@
+import { motion } from "framer-motion";
+import React from "react";
+import {
+  HtmlIcon,
+  ReactIcon,
+  JsIcon,
+  MySQLIcon,
+  PostgresSQLIcon,
+  NodeIcon,
+  MongoDbIcon,
+  NextJsIcon,
+  ReduxIcon,
+  ExpressJsIcon,
+  TailwindIcon,
+  CssIcom,
+} from "./Icons";
+
+const Skill = ({ icon, name, x, y }) => {
+  return (
+    <motion.div
+      className="flex items-center justify-center rounded-full font-semibold bg-dark text-light
+          py-3 px-6 shadow-dark cursor-pointer absolute"
+      whileHover={{ scale: 1.05 }}
+      initial={{ x: 0, y: 0 }}
+      whileInView={{ x: x, y: y }}
+      transition={{ duration: 1.5 }}
+    >
+      <div className="mr-1">{icon}</div>
+      {name}
+    </motion.div>
+  );
+};
+
+const Skills = () => {
+  return (
+    <>
+      <h2 className="font-bold text-8xl mt-64 w-full text-center">
+        Habilidades
+      </h2>
+      <div className="w-full h-screen relative flex items-center justify-center rounded-full bg-circularLight">
+        <motion.div
+          className="flex items-center justify-center rounded-full font-semibold bg-dark text-light
+          p-8 shadow-dark cursor-pointer"
+          whileHover={{ scale: 1.05 }}
+        >
+          Web
+        </motion.div>
+        <Skill icon={<HtmlIcon />} name="HTML" x="-20vw" y="2vw" />
+        <Skill icon={<CssIcom />} name="CSS" x="1vw" y="22vw" />
+        <Skill icon={<JsIcon />} name="JavaScript" x="20vw" y="6vw" />
+        <Skill icon={<ReactIcon />} name="ReactJS" x="0vw" y="10vw" />
+        <Skill icon={<NextJsIcon />} name="NextJS" x="-20vw" y="-15vw" />
+        <Skill icon={<ReduxIcon />} name="Redux" x="15vw" y="-12vw" />
+        <Skill icon={<NodeIcon />} name="NodeJS" x="32vw" y="-5vw" />
+        <Skill icon={<ExpressJsIcon />} name="ExpressJS" x="0vw" y="-22vw" />
+        <Skill icon={<MongoDbIcon />} name="MongoBD" x="-5vw" y="-10vw" />
+        <Skill
+          icon={<PostgresSQLIcon />}
+          name="PostgresSQL"
+          x="-25vw"
+          y="18vw"
+        />
+        <Skill icon={<MySQLIcon />} name="MySQL" x="30vw" y="18vw" />
+        <Skill icon={<TailwindIcon />} name="TailwindCSS" x="-35vw" y="-5vw" />
+      </div>
+    </>
+  );
+};
+
+export default Skills;
---
+++ src/pages/about.js
@@ -0,0 +1,117 @@
+import AnimatedText from "@/components/AnimatedText";
+import Layout from "@/components/Layout";
+import Head from "next/head";
+import Image from "next/image";
+import React, { useEffect, useRef } from "react";
+import profilePic from "../../public/images/profile/developer-pic-2.jpg";
+import { useInView, useMotionValue, useSpring } from "framer-motion";
+import Skills from "@/components/Skills";
+
+const AnimatedNumbers = ({ value }) => {
+  const ref = useRef(null);
+  const motionValue = useMotionValue(0);
+  const springValue = useSpring(motionValue, { duration: 3000 });
+  const isInView = useInView(ref, { once: true });
+
+  useEffect(() => {
+    if (isInView) {
+      motionValue.set(value);
+    }
+  }, [isInView, value, motionValue]);
+
+  useEffect(() => {
+    springValue.on("change", (latest) => {
+      if (ref.current && latest.toFixed(0) <= value) {
+        ref.current.textContent = latest.toFixed(0);
+      }
+    });
+  }, [springValue, value]);
+
+  return <span ref={ref}></span>;
+};
+const about = () => {
+  return (
+    <>
+      <Head>
+        <title>Antonio Rodríguez | About Page</title>
+        <meta name="description" content="any description" />
+      </Head>
+      <main className="flex w-full flex-col items-center justify-center">
+        <Layout className="pt-16">
+          <AnimatedText
+            text="La Pasión Alimenta El Propósito!"
+            className="mb-16"
+          />
+          <div className="grid w-full grid-cols-8 gap-16">
+            <div className="col-span-3 flex flex-col items-start justify-start">
+              <h2 className="mb-4 text-lg font-bold uppercase text-dark/75">
+                Sobre Mí
+              </h2>
+              <p className="font-medium">
+                ¡Hola! 👋 Soy Antonio Rodriguez, desarrollador Full Stack en
+                constante evolución y aprendizaje. Mi background en electrónica
+                y microsoldadura SMD ha desarrollado mi pensamiento lógico y
+                habilidades de resolución de problemas.
+              </p>
+              <p className="my-4 font-medium">
+                Disfruto desarrollando soluciones que satisfacen las necesidades
+                de clientes y usuarios. He participado en proyectos grupales,
+                donde he obtenido excelentes resultados gracias a mi capacidad
+                de aprendizaje y compromiso.
+              </p>
+              <p className="font-medium">
+                Espero con interés la oportunidad de aportar mis habilidades y
+                pasión a su próximo proyecto, contribuyendo a su éxito con mis
+                conocimientos y dedicación.
+              </p>
+            </div>
+
+            <div
+              className="col-span-3 relative h-max rounded-2xl border-2 border-solid border-dark
+            bg-light p-8"
+            >
+              <div className="absolute top-0 -right-3 -z-10 w-[102%] h-[103%] rounded-[2rem] bg-dark" />
+              <Image
+                src={profilePic}
+                alt="Antonio Rodríguez"
+                className="w-full h-auto rounded-2xl"
+              />
+            </div>
+
+            <div className="col-span-2 flex flex-col items-end justify-between">
+              <div className="flex flex-col items-end justify-center">
+                <span className="inline-block text-7xl font-bold">
+                  <AnimatedNumbers value={2} />+
+                </span>
+                <h2 className="text-xl font-medium capitalize text-dark/75">
+                  clientes
+                </h2>
+              </div>
+
+              <div className="flex flex-col items-end justify-center">
+                <span className="inline-block text-7xl font-bold">
+                  <AnimatedNumbers value={5} />+
+                </span>
+                <h2 className="text-xl font-medium capitalize text-dark/75">
+                  proyectos
+                </h2>
+              </div>
+
+              <div className="flex flex-col items-end justify-center">
+                <span className="inline-block text-7xl font-bold">
+                  <AnimatedNumbers value={8} />+
+                </span>
+                <h2 className="text-xl font-medium capitalize text-dark/75">
+                  experiencia (meses)
+                </h2>
+              </div>
+            </div>
+          </div>
+          <Skills />
+        </Layout>
+      </main>
+    </>
+  );
+};
+
+export default about;
