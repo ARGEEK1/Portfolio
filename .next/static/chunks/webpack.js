695f6611423b4dd452e8c3620fc23c357731210b
ARGEEK1 <antonio198889@gmail.com>
2023-06-02 18:16:11 +0200

    [ADD] Responsiveness

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "9f5d5a2f7e1acdf7"; }
+/******/ 		__webpack_require__.h = function() { return "e05be5352f32415c"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -22,6 +22,32 @@ const CustomLink = ({ href, title, className = "" }) => {
   );
 };
 
+const CustomMobileLink = ({ href, title, className = "", toggle }) => {
+  const router = useRouter();
+
+  const handleClick = () => {
+    toggle();
+    router.push(href);
+  };
+
+  return (
+    <button
+      href={href}
+      className={`${className} relative group text-light dark:text-dark my-2`}
+      onClick={handleClick}
+    >
+      {title}
+      <span
+        className={`h-[1.5px] inline-block bg-light dark:bg-dark absolute left-0 -bottom-1 group-hover:w-full transition-[width] ease duration-3000 ${
+          router.asPath === href ? "w-full" : "w-0"
+        }`}
+      >
+        &nbsp;
+      </span>
+    </button>
+  );
+};
+
 const MotionLink = motion(Link);
 
 const NavBar = () => {
@@ -36,68 +62,154 @@ const NavBar = () => {
   return (
     <header
       className="w-full px-32 py-8 font-medium flex items-center justify-between
-    dark:text-light"
+    dark:text-light relative"
     >
       <button
-        className="flex flex-col justify-center items-center"
+        className="flex-col justify-center items-center hidden lg:flex"
         onClick={handleClick}
       >
-        <span className="bg-dark dark:bg-light block h-0.5 w-6 rounded-sm -translate-y-0.5"></span>
         <span
-          className={`bg-dark dark:bg-light block h-0.5 w-6 rounded-sm my-0.5`}
+          className={`bg-dark dark:bg-light block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm ${
+            isOpen ? "rotate-45 translate-y-1" : "-translate-y-0.5"
+          }`}
         ></span>
-        <span className="bg-dark dark:bg-light block h-0.5 w-6 rounded-sm translate-y-0.5"></span>
+        <span
+          className={`bg-dark dark:bg-light block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm my-0.5 ${
+            isOpen ? "opacity-0" : "opacity-100"
+          }`}
+        ></span>
+        <span
+          className={`bg-dark dark:bg-light block transition-all duration-300 ease-out h-0.5 w-6 rounded-sm ${
+            isOpen ? "-rotate-45 -translate-y-1" : "translate-y-0.5"
+          }`}
+        ></span>
       </button>
 
-      <nav>
-        <CustomLink href="/" title="Inicio" className="mr-4" />
-        <CustomLink href="/about" title="Sobre Mí" className="mx-4" />
-        <CustomLink href="/projects" title="Proyectos" className="mx-4" />
-      </nav>
+      <div className="w-full flex justify-between items-center lg:hidden ">
+        <nav>
+          <CustomLink href="/" title="Inicio" className="mr-4" />
+          <CustomLink href="/about" title="Sobre Mí" className="mx-4" />
+          <CustomLink href="/projects" title="Proyectos" className="mx-4" />
+        </nav>
 
-      <nav className="flex items-center justify-center flex-wrap">
-        <motion.a
-          href="https://github.com/ARGEEK1"
-          target="_blank"
-          whileHover={{ y: -2 }}
-          whileTap={{ scale: 0.9 }}
-          className="w-6 mx-3"
-        >
-          <GitHubIcon />
-        </motion.a>
-        <motion.a
-          href="https://www.linkedin.com/in/antonio-rodr%C3%ADguez-toro/"
-          target="_blank"
-          whileHover={{ y: -2 }}
-          whileTap={{ scale: 0.9 }}
-          className="w-6 mx-3"
-        >
-          <LinkedInIcon />
-        </motion.a>
-        <MotionLink
-          href="mailto:antonio198889@gmail.com"
-          target="_blank"
-          title="Contáctame"
-          whileHover={{ y: -2 }}
-          whileTap={{ scale: 0.9 }}
-          className="w-6 mx-3"
-        >
-          <MailIcon />
-        </MotionLink>
+        <nav className="flex items-center justify-center flex-wrap">
+          <motion.a
+            href="https://github.com/ARGEEK1"
+            target="_blank"
+            whileHover={{ y: -2 }}
+            whileTap={{ scale: 0.9 }}
+            className="w-6 mx-3"
+          >
+            <GitHubIcon />
+          </motion.a>
+          <motion.a
+            href="https://www.linkedin.com/in/antonio-rodr%C3%ADguez-toro/"
+            target="_blank"
+            whileHover={{ y: -2 }}
+            whileTap={{ scale: 0.9 }}
+            className="w-6 mx-3"
+          >
+            <LinkedInIcon />
+          </motion.a>
+          <MotionLink
+            href="mailto:antonio198889@gmail.com"
+            target="_blank"
+            title="Contáctame"
+            whileHover={{ y: -2 }}
+            whileTap={{ scale: 0.9 }}
+            className="w-6 mx-3"
+          >
+            <MailIcon />
+          </MotionLink>
 
-        <button
-          onClick={() => setMode(mode === "light" ? "dark" : "light")}
-          className={`ml-3 flex items-center justify-center rounded-full p-1
+          <button
+            onClick={() => setMode(mode === "light" ? "dark" : "light")}
+            className={`w-6 ml-3 flex items-center justify-center rounded-full p-1 sm:mx-1
           ${mode === "light" ? "bg-dark text-light" : "bg-light text-dark"}
           `}
+          >
+            {mode === "dark" ? (
+              <MoonIcon className={"fill-dark"} />
+            ) : (
+              <SunIcon className={"fill-dark"} />
+            )}
+          </button>
+        </nav>
+      </div>
+
+      {isOpen ? (
+        <motion.div
+          initial={{ scale: 0, opacity: 0, x: "-50%", y: "-50%" }}
+          animate={{ scale: 1, opacity: 1 }}
+          className="min-w-[70vw] flex flex-col justify-between z-30 items-center fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2
+        bg-dark/90 dark:bg-light/75 rounded-lg backdrop-blur-md py-32"
         >
-          {mode === "dark" ? (
-            <MoonIcon className={"fill-dark"} />
-          ) : (
-            <SunIcon className={"fill-dark"} />
-          )}
-        </button>
-      </nav>
+          <nav className="flex items-center flex-col justify-center">
+            <CustomMobileLink
+              href="/"
+              title="Inicio"
+              className=""
+              toggle={handleClick}
+            />
+            <CustomMobileLink
+              href="/about"
+              title="Sobre Mí"
+              className=""
+              toggle={handleClick}
+            />
+            <CustomMobileLink
+              href="/projects"
+              title="Proyectos"
+              className=""
+              toggle={handleClick}
+            />
+          </nav>
+
+          <nav className="flex items-center justify-center flex-wrap mt-4">
+            <motion.a
+              href="https://github.com/ARGEEK1"
+              target="_blank"
+              whileHover={{ y: -2 }}
+              whileTap={{ scale: 0.9 }}
+              className="w-6 mx-3 text-light dark:text-dark sm:mx-1"
+            >
+              <GitHubIcon />
+            </motion.a>
+            <motion.a
+              href="https://www.linkedin.com/in/antonio-rodr%C3%ADguez-toro/"
+              target="_blank"
+              whileHover={{ y: -2 }}
+              whileTap={{ scale: 0.9 }}
+              className="w-6 mx-3 sm:mx-1"
+            >
+              <LinkedInIcon />
+            </motion.a>
+            <MotionLink
+              href="mailto:antonio198889@gmail.com"
+              target="_blank"
+              title="Contáctame"
+              whileHover={{ y: -2 }}
+              whileTap={{ scale: 0.9 }}
+              className="w-6 mx-3 text-light dark:text-dark sm:mx-1"
+            >
+              <MailIcon />
+            </MotionLink>
+
+            <button
+              onClick={() => setMode(mode === "light" ? "dark" : "light")}
+              className={`w-6 ml-3 flex items-center justify-center rounded-full p-1 sm:mx-1 ${
+                mode === "light" ? "bg-light text-dark" : "bg-dark text-light"
+              }`}
+            >
+              {mode === "dark" ? (
+                <MoonIcon className={"fill-dark"} />
+              ) : (
+                <SunIcon className={"fill-dark"} />
+              )}
+            </button>
+          </nav>
+        </motion.div>
+      ) : null}
 
       <div className="absolute left-[50%] top-2 translate-x-[-50%]">
         <Logo />
